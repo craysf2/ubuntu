1c8c00ba6377e758aa115f42981a2018dfc7f693
craysf2 <119637152+craysf2@users.noreply.github.com>
2022-12-05 23:47:58 -0800

    Add files via upload

---
+++ skripta2.sh
@@ -0,0 +1,46 @@
+mkdir -p struktura/{desktop, documents, downloads, pictures, videos}
+
+cd struktura/documents
+
+for i in {1..5}
+do
+mkdir folder$i
+done
+
+for i in {1..5}
+do
+useradd -m --groups sudo user$i
+done
+
+sudo apt update
+
+sudo apt-get install net-tools
+
+sudo apt-get update
+
+sudo apt-get install git
+
+sudo apt-get update
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo docker run hello-world
+
+
+
+
